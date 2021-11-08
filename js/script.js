25eb99022d2f816210226e91f687a75c6e797cce
JONATAS NASCIMENTO DOS SANTOS <21193533@CFP127ALUNO.LOCAL>
2021-11-08 16:53:26 -0300

    adicionando o js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 "use stritic";
 
 
+const limparElementos = (elemento) =>{ // 12 passo - criar uma funcao para limpar
+    while(elemento.firstChild){ // equanto tiver o primeiro elemento remove o ultimo, limpa toda div container para garregar denovo a div
+        elemento.removeChild(elemento.lastChild);
+    }
+
+}
+
+
 const pesquisar =async(evento) =>{
-    if(evento.key == 'Enter'){
+    if(evento.key == 'Enter' || evento.type == 'click'){
         const pesquisa = document.getElementById('pesquisa').value;
+       
         // console.log(pesquisa);
         const url = `https://pixabay.com/api/?key=24217094-d3bf941cb7e41a3f57527f311&q=${pesquisa}&image_type=photo`;
         // console.log(url);
         const dados = await fetch(url); 
         // console.log(dados);
         const pesquisar = await dados.json();
-         console.log(pesquisar.hits);
 
+
+        limparElementos(document.querySelector('.galeria'));
+        //  console.log(pesquisar);
+         carregarGaleria(pesquisar.hits); 
         
         
     }
@@ -19,5 +31,45 @@ const pesquisar =async(evento) =>{
 
 }
 
-document.getElementById('pesquisa')
-        .addEventListener('keypress', pesquisar);
+
+
+    carregarImagem = (item) =>{
+        // const imagemPesquisa = document.getElementById("imagemPesquisa");
+        const galeria = document.querySelector('.galeria');
+
+        const novoDiv = document.createElement("div")
+        novoDiv.classList.add("ss") 
+        novoDiv.innerHTML = 
+        ` <div class="imagem-container">
+        <img class= "imgPreview" src="${item.previewURL}">
+    
+       </div>  
+       <div>
+       <div class="tags">
+    
+       ${item.tags}
+       
+       </div>
+       <div class="likes">
+       <div>
+        <img class="imgLikes" src="img/like.png">
+       </div>
+          ${item.likes}
+        
+       </div>
+       <div class="comments">${item.comments} </div>
+       </div>
+    `
+         galeria.appendChild(novoDiv)
+        
+    }
+
+const carregarGaleria = (imagens) => imagens.forEach(carregarImagem)
+
+
+
+
+document.getElementById('pesquisa').addEventListener('keypress', pesquisar);
+
+         document.getElementById('imagemPesquisa').onclick =  pesquisar;
+        // imagemPesquisa.addEventListener('click', carregarImagem);
